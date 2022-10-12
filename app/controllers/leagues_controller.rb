54ae0d3fac47b4bcf7e8a8cb3f0b3d12281b63ec
Peter <babyghost@gmail.com>
2022-10-12 02:50:07 -0500

    Added the simple show page to show some data.

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -2,4 +2,8 @@ class LeaguesController < ApplicationController
   def index
     @leagues = League.includes(:country, :type).all
   end
+
+  def show
+    @league = League.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "leagues#index"
-  get "/leagues", to: "leagues#index"
+
+  resources :leagues, :countries, :types
+
   get "/about", to: "about#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
