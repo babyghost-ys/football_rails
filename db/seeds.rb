90c0d19b348daaa11dbe818db72ee9f39489ca93
Peter <babyghost@gmail.com>
2022-10-12 02:09:12 -0500

    Updated the db:seeds file for testing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
+# Clear all the tables first
+League.delete_all
+Type.delete_all
+Country.delete_all
+puts "Deleted all table"
+
+# Function to fetch data
+def fetch_data(urlString)
+  url = URI(urlString)
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Get.new(url)
+  request["X-RapidAPI-Key"] = 'RZFbtPzPjymshBTePpvz3NzvYJHGp175bvxjsnWj7QxJfn7dza'
+  request["X-RapidAPI-Host"] = 'api-football-v1.p.rapidapi.com'
+
+  response = http.request(request)
+  content = JSON.parse(response.read_body)
+
+  return content
+end
+
+# Main code to fetch the API
+returned_content = fetch_data("https://webdev2.winandmac.com/sample.json")
+leagues = returned_content["response"]
+
+leagues.each do |league|
+  type = Type.find_or_create_by(name: league["league"]["type"])
+  country = Country.find_or_create_by(name: league["country"]["name"])
+
+  # Find out the id so that we can add that to the League table correctly
+  current_type_id = Type.where({name: league["league"]["type"]}).first.id
+
+  country.leagues.create(name: league["league"]["name"], type_id: current_type_id)
+  #puts league["league"]["name"]
+  #puts league["league"]["logo"]
+  #puts league["country"]["name"]
+  #puts league["seasons"][0]["start"]
+  #puts league["league"]["type"]
+end
