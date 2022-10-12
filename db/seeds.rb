cc690a835f55a6c8ad8b3a76e31f90dd4c87aefd
Peter <babyghost@gmail.com>
2022-10-12 02:16:22 -0500

    Updated the db:seeds file again to fix the errors while pouring data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,17 +31,20 @@ end
 returned_content = fetch_data("https://webdev2.winandmac.com/sample.json")
 leagues = returned_content["response"]
 
+# Loop through the returned items
 leagues.each do |league|
+
+  # Create the type and country first
   type = Type.find_or_create_by(name: league["league"]["type"])
   country = Country.find_or_create_by(name: league["country"]["name"])
 
   # Find out the id so that we can add that to the League table correctly
   current_type_id = Type.where({name: league["league"]["type"]}).first.id
 
-  country.leagues.create(name: league["league"]["name"], type_id: current_type_id)
-  #puts league["league"]["name"]
-  #puts league["league"]["logo"]
-  #puts league["country"]["name"]
-  #puts league["seasons"][0]["start"]
-  #puts league["league"]["type"]
+  # Create the associated item to the League table by country object
+  country.leagues.create(
+    name: league["league"]["name"],
+    logo: league["league"]["logo"],
+    season: league["seasons"][0]["start"],
+    type_id: current_type_id)
 end
