2556535a1dc37d0bb000add511200cfeded60ab7
Peter <babyghost@gmail.com>
2022-10-13 23:33:08 -0500

    Added the players controller and related pages to show the many-to-many relationship.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,4 @@
 class Player < ApplicationRecord
   has_many :player_leagues, dependent: :delete_all
-  has_many :players, through: :player_leagues, dependent: :delete_all
+  has_many :leagues, through: :player_leagues, dependent: :delete_all
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'types/index'
   root "leagues#index"
 
-  resources :leagues, :countries, :types
+  resources :leagues, :countries, :types, :players
 
   get "/about", to: "about#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
